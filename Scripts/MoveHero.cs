95e6c6799a9f5b47c408edbb7e5d6b7445871551
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-05-11 09:38:26 +0300

    Fix infitini jumping

--- Scripts/MoveHero.cs
+++ Scripts/MoveHero.cs
@@ -6,6 +6,7 @@ public class MoveHero : MonoBehaviour
 {
     public float speed = 1f; // швидкість руху героя
     public float jump = 4f; // висота стрибка
+    private bool isGrounded;
 
     SpriteRenderer sr;
     Rigidbody2D rb;
@@ -27,11 +28,21 @@ public class MoveHero : MonoBehaviour
         // переміщуємо героя, використовуючи швидкість [speed]
         transform.position += new Vector3(move, 0, 0) * speed * Time.deltaTime;
 
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
             rb.AddForce(new Vector3(0, jump), ForceMode2D.Impulse);
 
         if (move < 0) sr.flipX = true;
         
         if (move > 0) sr.flipX = false;
+    }
+
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+
+    public void OnCollisionExit2D(Collision2D other)
+    {
+        isGrounded = false;
     }
 }
