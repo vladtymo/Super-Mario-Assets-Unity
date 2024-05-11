03cb5b83dac25d1c841adc975f4866c1a969bd04
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-05-11 09:45:02 +0300

    Fix coin scritp

--- Scripts/Coin.cs
+++ Scripts/Coin.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision) 
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         gameObject.SetActive(false);
     }
